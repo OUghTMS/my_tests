ac370aa73d07334df25a264d79d3591338b86089
OUghTMS <misha.saan@gmail.com>
2020-07-02 09:57:26 +0300

    Updates

---
+++ precache-manifest.0185588df24719c856bd43b3f2a79371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f95b150602987d487984f9ffd95946",
+    "url": "/my_tests/index.html"
+  },
+  {
+    "revision": "676476ec44a13c91e5f4",
+    "url": "/my_tests/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d473068412378b74034",
+    "url": "/my_tests/static/js/2.df92f47e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_tests/static/js/2.df92f47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676476ec44a13c91e5f4",
+    "url": "/my_tests/static/js/main.26c9c3ae.chunk.js"
+  },
+  {
+    "revision": "2b3829ae8bd12e0b3de1",
+    "url": "/my_tests/static/js/runtime-main.1d859bcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_tests/static/media/logo.5d5d9eef.svg"
+  }
+]);
